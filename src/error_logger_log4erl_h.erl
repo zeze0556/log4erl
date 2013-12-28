68368732f54c6d4a810b54e57184173f60ef5c26
--==RIX==-- <zeze0556@gmail.com>
2013-12-28 14:10:54 +0000

    为log4erl添加对error_report, info_report, warning_report的支持

--- src/error_logger_log4erl_h.erl
+++ src/error_logger_log4erl_h.erl
@@ -66,12 +66,24 @@ handle_event({info_msg, _GLeader, {_PID, Msg, Data}}, #elogger_l4e_mappings{info
 handle_event({warning_msg, _GLeader, {_PID, Msg, Data}}, #elogger_l4e_mappings{warning_msg=L} = State) ->
     R = log4erl:log(L, Msg, Data),
     {R, State};
-handle_event({error_report, _GLeader, _}, State) ->
-    {ok, State};
-handle_event({info_report, _GLeader, _}, State) ->
-    {ok, State};
-handle_event({warning_report, _GLeader, _}, State) ->
-    {ok, State}.
+handle_event({error_report, _GLeader, {_PID, crash_report, Data}}, #elogger_l4e_mappings{error_report=L} = State) ->
+    R = log4erl:log(L, format_and_prefix_report("CRASH REPORT", Data)),
+    {R, State};
+handle_event({error_report, _GLeader, {_PID, supervisor_report, Data}}, #elogger_l4e_mappings{error_report=L} = State) ->
+    R = log4erl:log(L, format_and_prefix_report("SUPERVISOR REPORT", Data)),
+    {R, State};
+handle_event({error_report, _GLeader, {_PID, _Type, Data}}, #elogger_l4e_mappings{error_report=L} = State) ->
+    R = log4erl:log(L, format_and_prefix_report("ERROR REPORT", Data)),
+    {R, State};
+handle_event({info_report, _GLeader, {_Pid, progress, Rep}}, #elogger_l4e_mappings{info_report=L} = State) ->
+    R = log4erl:log(L, format_and_prefix_report("PROGRESS REPORT", Rep)),
+    {R, State};
+handle_event({info_report, _GLeader, {_Pid, _Type, Rep}}, #elogger_l4e_mappings{info_report = L} = State) ->
+    R = log4erl:log(L, format_and_prefix_report("INFO REPORT", Rep)),
+    {R, State};
+handle_event({warning_report, _GLeader, {_Pid, _Type, Rep}}, #elogger_l4e_mappings{warning_report = L} = State) ->
+    R = log4erl:log(L, format_and_prefix_report("WARNING REPORT", Rep)),
+    {R, State}.
 
 handle_call(_Request, State) ->
     Reply = ok,
@@ -85,3 +97,49 @@ terminate(_Reason, _State) ->
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
+%%%-----------------------------------------------------------------
+%%% Utility code taken from the erlang error_logger source.
+%%%-----------------------------------------------------------------
+
+format_and_prefix_report(Name, Rep) ->
+  io_lib:format("[~s]~n~s", [Name, format_report(Rep)]).
+
+format_report(Rep) when is_list(Rep) ->
+  case string_p(Rep) of
+    true ->
+      io_lib:format("~s~n",[Rep]);
+    _ ->
+      format_rep(Rep)
+  end;
+format_report(Rep) ->
+  io_lib:format("~p~n",[Rep]).
+
+format_rep([{Tag,Data}|Rep]) ->
+  io_lib:format(" ~p: ~p~n",[Tag,Data]) ++ format_rep(Rep);
+format_rep([Other|Rep]) ->
+  io_lib:format(" ~p~n",[Other]) ++ format_rep(Rep);
+format_rep(_) ->
+  [].
+
+string_p([]) ->
+  false;
+string_p(Term) ->
+  string_p1(Term).
+
+string_p1([H|T]) when is_integer(H), H >= $\s, H < 255 ->
+  string_p1(T);
+string_p1([$\n|T]) -> string_p1(T);
+string_p1([$\r|T]) -> string_p1(T);
+string_p1([$\t|T]) -> string_p1(T);
+string_p1([$\v|T]) -> string_p1(T);
+string_p1([$\b|T]) -> string_p1(T);
+string_p1([$\f|T]) -> string_p1(T);
+string_p1([$\e|T]) -> string_p1(T);
+string_p1([H|T]) when is_list(H) ->
+  case string_p1(H) of
+    true -> string_p1(T);
+    _ -> false
+  end;
+string_p1([]) -> true;
+string_p1(_) -> false.
+
